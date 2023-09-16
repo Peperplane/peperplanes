edc4fe864734c9890419fa8be4679c20efb9b6e8
Anusuri Sai Durga Prasad <145194427+Peperplane@users.noreply.github.com>
2023-09-16 17:59:41 +0530

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns # new
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('',include('utube.urls')),
 ]
 urlpatterns += staticfiles_urlpatterns() # new
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
